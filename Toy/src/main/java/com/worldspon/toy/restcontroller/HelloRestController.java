bc1320595e391d7ef450f96d6876ab1f3073926e
Johnny <Johnny@DESKTOP-N7IG9E9>
2019-01-04 11:36:58 +0900

    author: Johnny | message: jsp 템플릿 설정 및 정적 파일 업로드(js, css)

--- Toy/src/main/java/com/worldspon/toy/restcontroller/HelloRestController.java
+++ Toy/src/main/java/com/worldspon/toy/restcontroller/HelloRestController.java
@@ -2,6 +2,7 @@ package com.worldspon.toy.restcontroller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HelloRestController {
@@ -9,5 +10,12 @@ public class HelloRestController {
 	@GetMapping("/hello")
 	public String hello() throws Exception {
 		return "Hello world!";
+	}
+	
+	@GetMapping("/")
+	public ModelAndView main() throws Exception {
+		ModelAndView mav = new ModelAndView("main");
+		mav.addObject("message", "MAIN PAGE!");
+		return mav;
 	}
 }
