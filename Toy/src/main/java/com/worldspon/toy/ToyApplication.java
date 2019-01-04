fd165455d197657eadf7aa4deb36afde456f97b8
Johnny <Johnny@DESKTOP-N7IG9E9>
2019-01-04 10:46:02 +0900

    author: Johnny | Message: 프로젝트 최초 커밋

---
+++ Toy/src/main/java/com/worldspon/toy/ToyApplication.java
@@ -0,0 +1,16 @@
+package com.worldspon.toy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing	// JPA Auditing 활성화
+@SpringBootApplication
+public class ToyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyApplication.class, args);
+	}
+
+}
+
